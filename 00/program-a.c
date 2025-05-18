6e4a0977d2c15388a2b6f128e1e15dd7c7d8a475
starry_tree <starry_tree@outlook.com>
2025-05-18 11:10:25 +0800

    00

---
+++ 00/program-a.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <unistd.h>
+
+int main(){
+    sleep(5);
+
+    const char* hello_string = "Hello Linux!\n";
+
+    printf(hello_string);
+
+    FILE *output_file = fopen("output.txt", "w");
+    fputs(hello_string, output_file);
+    fclose(output_file);
+
+    return 0;
+}
